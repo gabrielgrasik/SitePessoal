edc6d106361d1745f625334ca043f069a50c4cbd
gabrielgrasik <gabrielgrasik@gmail.com>
2024-09-29 17:38:05 -0300

    Projeto

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('message-btn').addEventListener('click', function() {
+    alert("Olá, eu sou Gabriel, Estudande de Ciências da Computação e apaixonado por tecnologia");
+});
